8e559ef273b15dceac3c03b770c5a05363760eac
AI-nix <siztic@gmail.com>
2025-04-07 15:59:18 +0900

    main수정

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-# main.py
 from summarizer import summarize_news
 from mailer import send_email
+from news_fetcher import get_ai_news # 뉴스 크롤링 함수
 
 if __name__ == '__main__':
+    # 뉴스 데이터 가져오기
+    articles = get_ai_news()
+
+    # 뉴스 요약
     news_list = summarize_news(articles)
 
     # Markdown 스타일 텍스트 요약 생성
@@ -24,4 +28,5 @@ if __name__ == '__main__':
         summary_html += "</li>"
     summary_html += "</ol>"
 
+    # 이메일 전송
     send_email("[오늘의 AI 뉴스 요약]", summary, summary_html)
